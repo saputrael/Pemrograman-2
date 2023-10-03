6b2ecbb6d5e0b28d7558c63b985e9ba5e8745a5d
saputrael <119580576+saputrael@users.noreply.github.com>
2023-10-03 11:45:47 +0700

    Add files via upload

---
+++ Tugas.js
@@ -0,0 +1,8 @@
+const firstName = "Aryo";
+const lastName = "Galuh";
+const age = 20;
+const isMarried = false;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried); 
